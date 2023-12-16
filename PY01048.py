f13d4c12c244036b0843c768597417a832adee43
MSN <108249986+QuyetFCB275@users.noreply.github.com>
2023-12-16 23:24:05 +0700

    Update PY01048.py

--- PY01048.py
+++ PY01048.py
@@ -3,9 +3,15 @@ from math import *
 for i in range(int(input())):
     n = int(input())
     ans = 0
-    for j in range(2, isqrt(2*n) + 1):
+    for j in range(2, isqrt(2*n)+1):
         if (j % 2 == 0 and n / j == n//j + 0.5) or (j % 2 == 1 and n % j == 0):
             k = n / j
-            if j / 2 < k:
+            if j / 2 <= k:
                 ans += 1
     print(ans)
+
+"""
+ Hướng giải quyết:
+ Phân tích n ra một dãy các số tự nhiên liên tiếp (giả sử N số). Khi đó n = N * value_TB
+ N sẽ là chẵn hoặc lẻ. Nếu N chẵn thì value_TB sẽ là lẻ 0.5, nếu N lẻ thì n % N == 0
+"""
