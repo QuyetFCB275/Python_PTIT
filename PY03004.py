942b04b092017235afbe172e1e1fc2a4cf1c3a93
MSN <108249986+QuyetFCB275@users.noreply.github.com>
2023-12-17 11:01:47 +0700

    Update PY03004.py

--- PY03004.py
+++ PY03004.py
@@ -22,3 +22,34 @@ for items in a.items():
 c.sort(key=lambda x : (-x[1], x[0]))
 for x in c:
     print(x[0], x[1])
+
+# Solution2:
+'''
+import re
+
+a = []
+dt = {}
+
+def check(s: str):
+    for x in s:
+        if not x.isalnum():
+            return False
+    return True
+
+
+for i in range(int(input())):
+    text = input().strip().lower()
+    b = re.split('[ ,.?!:;)(-/]', text)
+    for x in b:
+        if len(x) > 0 and check(x):
+            if x in dt:
+                dt[x] += 1
+            else:
+                dt[x] = 1
+
+for x in dt.items():
+    a.append(x)
+a.sort(key=lambda x: (-x[1], x[0]))
+for x in a:
+    print(x[0], x[1])
+'''
